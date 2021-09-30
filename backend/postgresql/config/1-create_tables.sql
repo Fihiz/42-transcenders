1d6db9eea3ed93ea68ea035b380152f5bff0cd50
Salome D'audeteau <sad-aude@z4r4p7.42lyon.fr>
2021-09-30 17:33:31 +0200

    setting a docker-compose version that currently runs 4 detached services (angular, nestjs, postgresql + adminer to do some tests), need to go on a .env and be sure that everything works on each session

---
+++ backend/postgresql/config/1-create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS utilisateur
+(
+    id INT PRIMARY KEY NOT NULL,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    email VARCHAR(255),
+    date_naissance DATE,
+    pays VARCHAR(255),
+    ville VARCHAR(255),
+    code_postal VARCHAR(5),
+    nombre_achat INT
+)
