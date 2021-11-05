f4b4bdd7b9beaa5557f42b80d6ff63c7a7c74444
Agathe Geraud <ageraud@z2r7p6.42lyon.fr>
2021-11-05 18:23:47 +0100

    starting router, creation of app-routing.modul.ts

--- tourOfHeroes/src/app/app-routing.module.ts
+++ tourOfHeroes/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class AppRoutingModule { }
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- tourOfHeroes/src/app/app.module.ts
+++ tourOfHeroes/src/app/app.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
