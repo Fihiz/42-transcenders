5005ba6f6b82ce23ffd2e0ef095155c448f47015
Agathe Geraud <ageraud@z4r5p7.42lyon.fr>
2021-11-08 18:35:25 +0100

    search ny name OK - tuto is done

--- tourOfHeroes/src/app/app.module.ts
+++ tourOfHeroes/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { InMemoryDataService } from './in-memory-data.service';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
--- tourOfHeroes/src/app/hero.service.ts
+++ tourOfHeroes/src/app/hero.service.ts
@@ -93,4 +93,19 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((x) =>
+        x.length
+          ? this.log(`found heroes matching "${term}"`)
+          : this.log(`no heroes matching "${term}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
