b47246e45b0e1dbcce447562b1f4dfcb50b401fe
Fihiz <salome.d.audeteau@gmail.com>
2021-11-10 18:59:25 +0100

    header and sidebar are now working from registered-page component, need to go on our components structure + replace the log out button...

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AuthPageComponent } from './auth-page/auth-page.component';
 import { RegisteredPageComponent } from './registered-page/registered-page.component';
 import { SidebarComponent } from './registered-page/sidebar/sidebar.component';
+import { HeaderComponent } from './registered-page/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthPageComponent,
     RegisteredPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
