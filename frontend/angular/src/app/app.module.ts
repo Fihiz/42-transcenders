5195c451a8a9ab93ba3ff8f8db5b20e22706d13d
Agat.inha <agathe.geraud@hotmail.fr>
2021-11-10 17:25:29 +0100

    add logOut button, banner 42 and register is centered, need to add sidebar and header in registered-page

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthPageComponent } from './auth-page/auth-page.component';
+import { RegisteredPageComponent } from './registered-page/registered-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthPageComponent
+    AuthPageComponent,
+    RegisteredPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/angular/src/app/registered-page/registered-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter  } from '@angular/core';
+
+@Component({
+  selector: 'app-registered-page',
+  templateUrl: './registered-page.component.html',
+  styleUrls: ['./registered-page.component.css']
+})
+export class RegisteredPageComponent {
+
+	@Input() isAuthCheck!: boolean;
+	@Output() isAuthChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+	onLogOut () {
+		this.isAuthChange.emit(this.isAuthCheck = false);
+	}
+
+}
