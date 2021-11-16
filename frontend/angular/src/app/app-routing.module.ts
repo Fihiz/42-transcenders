8e8f85d2aa470ff7f110065707e9f890cfab4745
Agathe Geraud <ageraud@z4r4p3.42lyon.fr>
2021-11-16 12:39:29 +0100

    routing component is ok for ruls and welcome page. it is still a mystery about how to have 2 different routing names

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RulesComponent } from './registered-page/content/rules/rules.component';
+import { WelcomeComponent } from './registered-page/content/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'pong/rules', component: RulesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
