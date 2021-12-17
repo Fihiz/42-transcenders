4ebe7c2f62f0bb7d93992df9927070b230669b5d
Agathe Geraud <ageraud@z4r4p3.42lyon.fr>
2021-12-17 17:47:45 +0100

    on the way to websockets

--- backend/nestjs/src/gateways/chat.gateway.ts
+++ backend/nestjs/src/gateways/chat.gateway.ts
@@ -1,28 +1,47 @@
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { MessageDto } from "src/dtos/messages.dto";
-import { ChatServiceBis } from "src/services/sb-chat-bis.service";
-import { ChatService } from "src/services/sb-chat.service";
-import { GlobalDataService } from "src/services/sb-global-data.service";
+// import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+// import { MessageDto } from "src/dtos/messages.dto";
+// import { ChatServiceBis } from "src/services/sb-chat-bis.service";
+// import { ChatService } from "src/services/sb-chat.service";
+// import { GlobalDataService } from "src/services/sb-global-data.service";
 
-@WebSocketGateway({cors:{origin: 'http://127.0.0.1'}})
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	conv_id: number = 0;
+// @WebSocketGateway({cors:{origin: 'http://127.0.0.1'}})
 
-	constructor(private chatService: ChatService,
-				private chatServiceBis: ChatServiceBis){}
+
+// export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+// 	// conv_id: number = 0;
+
+// 	// constructor(private chatService: ChatService,
+// 	// 			private chatServiceBis: ChatServiceBis){}
+// 	// @WebSocketServer()
+// 	// server;
+
+// 	// handleConnection() {
+// 	// 		console.log('chat connected')
+// 	// 	}
+
+// 	// handleDisconnect() {
+// 	// 	console.log('chat disconnection');
+// 	// }
+
+// 	// @SubscribeMessage('message')
+// 	// messageFunc(@MessageBody() message: MessageDto) {
+// 	// 	// this.chatService.handleMessage(this.server,message);
+// 	// }
+// }
+
+
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway() //cors
+export class ChatGateway {
+
 	@WebSocketServer()
 	server;
 
-	handleConnection() {
-			console.log('chat connected')
-		}
-
-	handleDisconnect() {
-		console.log('chat disconnection');
+	@SubscribeMessage('sm-message')
+	handleMassage(@MessageBody() messagebody: string) : void {
+		//emit-to all
+		this.server.emit('sm-message', messagebody);
 	}
 
-	@SubscribeMessage('message')
-	messageFunc(@MessageBody() message: MessageDto) {
-		// this.chatService.handleMessage(this.server,message);
-	}
 }
