22a768497caa155b9931a1a064d11d429d50862b
Fihiz <salome.d.audeteau@gmail.com>
2021-12-21 16:29:54 +0300

    pushing for now, leaving a mess of comments and annotations : border and color changed for right-col of registered-page to get an easier visibility + b-messaged has been changed for a max-height of 65% to not have the send input bar scrollable on my mac + a lot of colors changed to get a better visibility as well... + lot of comments for gateway logical, integration of the video effect step by step, front and back communicate by click on handleSubmitNewMessage thanks to the 'message' subscribe..., let's go'

--- backend/nestjs/src/gateways/chat.gateway.ts
+++ backend/nestjs/src/gateways/chat.gateway.ts
@@ -1,28 +1,45 @@
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { MessageDto } from "src/dtos/messages.dto";
-import { ChatServiceBis } from "src/services/sb-chat-bis.service";
-import { ChatService } from "src/services/sb-chat.service";
-import { GlobalDataService } from "src/services/sb-global-data.service";
+//import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+//import { MessageDto } from "src/dtos/messages.dto";
+//import { ChatServiceBis } from "src/services/sb-chat-bis.service";
+//import { ChatService } from "src/services/sb-chat.service";
+//import { GlobalDataService } from "src/services/sb-global-data.service";
 
-@WebSocketGateway({cors:{origin: 'http://127.0.0.1'}})
-export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-	conv_id: number = 0;
+//@WebSocketGateway({cors:{origin: 'http://127.0.0.1'}})
+//export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+//	conv_id: number = 0;
 
-	constructor(private chatService: ChatService,
-				private chatServiceBis: ChatServiceBis){}
+//	constructor(private chatService: ChatService,
+//				private chatServiceBis: ChatServiceBis){}
+//	@WebSocketServer()
+//	server;
+
+//	handleConnection() {
+//			console.log('chat connected')
+//		}
+
+//	handleDisconnect() {
+//		console.log('chat disconnection');
+//	}
+
+//	@SubscribeMessage('message')
+//	messageFunc(@MessageBody() message: MessageDto) {
+//		// this.chatService.handleMessage(this.server,message);
+//	}
+//}
+
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway({cors:{origin: 'http://127.0.0.1'}}) //cors
+export class ChatGateway {
+
+	inc : number = 0;
 	@WebSocketServer()
 	server;
 
-	handleConnection() {
-			console.log('chat connected')
-		}
-
-	handleDisconnect() {
-		console.log('chat disconnection');
-	}
-
-	@SubscribeMessage('message')
-	messageFunc(@MessageBody() message: MessageDto) {
-		// this.chatService.handleMessage(this.server,message);
+	@SubscribeMessage('message') // Client emit a message
+	handleMassage(@MessageBody() content: string) : void {
+		//emit to all
+		console.log(this.inc++, "Je passe dans le subscribe message");
+		this.server.emit('message', content); // We broadcast it to the server
 	}
 }
