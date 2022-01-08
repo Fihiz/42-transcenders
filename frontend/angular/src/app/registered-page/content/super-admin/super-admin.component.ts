1158de1d1ce2615b8682b9038514ef243ebaea62
Agathe Geraud <ageraud@z4r4p3.42lyon.fr>
2022-01-08 20:52:44 +0100

    ADMIN PANEL: added 2 btn , set as Moderator + set as user

--- frontend/angular/src/app/registered-page/content/super-admin/super-admin.component.ts
+++ frontend/angular/src/app/registered-page/content/super-admin/super-admin.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-super-admin',
   templateUrl: './super-admin.component.html',
-  styleUrls: ['./super-admin.component.css']
+  styleUrls: ['./super-admin.component.css'],
 })
 export class SuperAdminComponent implements OnInit {
-
-  constructor() { }
+  // allUsers;
+  constructor(private socket: Socket) {}
 
   ngOnInit(): void {
+    // this.socket.on('allUsers', (data: any) => {
+    // console.log('allAvailableRoomsInApp', data);
+    // this.listAllAvailableRooms = data;
+    // });
   }
-
 }
